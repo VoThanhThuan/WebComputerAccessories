ad5cb753259c672b36972e62855f12c3eeb0f836
Nguyen Ngoc Son <65350169+nnson19pm@users.noreply.github.com>
2021-06-05 22:40:38 +0700

    up

--- WebComputerAccessories/Controllers/AdditionnalCartController.cs
+++ WebComputerAccessories/Controllers/AdditionnalCartController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Data.Entity;
+using WebComputerAccessories.Models;
 namespace WebComputerAccessories.Controllers
 {
     public class AdditionnalCartController : Controller
     {
+        private WebAccessoriesModel db = new WebAccessoriesModel();
         // GET: AdditionnalCart
         public ActionResult Index()
         {
             return View();
         }
+
     }
 }
