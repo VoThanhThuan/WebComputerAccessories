7d9d8efa822b8095f57c55d7669e8531a3eb31c7
Võ Thành Thuận <voanome@gmail.com>
2021-06-06 01:12:53 +0700

    add cart, fix footer.

---
+++ WebComputerAccessories/Controllers/CartController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.IO;
+using WebComputerAccessories.Models;
+namespace WebComputerAccessories.Controllers
+{
+    public class CartController : Controller
+    {
+        private WebAccessoriesModel db = new WebAccessoriesModel();
+
+        // GET: AdditionnalCart
+        [Route("{id}")]
+        public ActionResult Index(Guid? id)
+        {
+            if(id == null)
+                return RedirectToAction("Index", "Home");
+            var product = db.Products.Find(id);
+            return View(product);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
