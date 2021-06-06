17d56f40f76806159dfdf4e63f086339bf5fd17f
Võ Thành Thuận <voanome@gmail.com>
2021-06-06 21:10:12 +0700

    thanh toán.

---
+++ WebComputerAccessories/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebComputerAccessories.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Base
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (Session["MaNguoiDung"] == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary(new { action = "Index", controller = "Home" }));
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
